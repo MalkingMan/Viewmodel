b955f7ae59d5ee7806a54d84b4c1489cf6311d24
MalkingMan <arrayalkhozini@gmail.com>
2025-11-20 13:08:15 +0700

    Add TampilSiswa composable with UI state parameter
    - Create `TampilSiswa.kt` in the `uicontroller` package.
    - Define `TampilSiswa` composable function accepting `statusUiSiswa` of type `Siswa` and an `onBackButtonClicked` callback.

---
+++ app/src/main/java/com/example/myarchitechture/view/uicontroller/TampilSiswa.kt
@@ -0,0 +1,9 @@
+package com.example.myarchitechture.view.uicontroller
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilSiswa(
+    // edit 1 : parameter statusUiSiswa
+    statusUiSiswa: Siswa,
+    onBackButtonClicked: () -> Unit
+) {
